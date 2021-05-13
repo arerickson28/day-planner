586d335ce1eb375d09ac9188ca5869f9ab6bcbd7
Austen Rhyce Erickson <a.r.erickson.28@gmail.com>
2021-05-13 11:52:12 -0500

    I got my page to interact as wished with local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,9 @@
-let DateTime = luxon.DateTime ;
-
 function displayTime() {
     let timeDisplayEl = $('#time-display');
 
     let currentDay = $('#currentDay')
+
+    let DateTime = luxon.DateTime ;
 
     let now = DateTime.now()
 
@@ -29,17 +29,7 @@ function displayTime() {
 
     month = monthsOfTheYear[month -1]
 
-
-    // let timeObject = {
-    //     "year": year,
-    //     "month": month,
-    //     "day": day,
-    //     "weekday": weekday,
-    //     "hour": hour,
-    //     "minute": minute,
-    //     "second": second
-    // }
-
+//---------------------------------------------------
     function getOrdinal() {
         let  dayAsString = String(day) ;
         let endOfDayString = dayAsString.charAt(dayAsString.length -1)
@@ -57,6 +47,7 @@ function displayTime() {
        return ordinal
     } 
    
+//---------------------------------------------------
     
     let chosenOrdinal = getOrdinal() ;
     
@@ -69,6 +60,7 @@ function displayTime() {
 
     let timeBlockArray = $(".event") ;
     for (let i = 0; i < timeBlockArray.length; i++ ){
+
         timeBlock = timeBlockArray.eq(i) ;
         timeBlockHead = timeBlock.siblings().first() ;
         timeBlockHeadValue = timeBlockHead[0].getAttribute("value");
@@ -78,7 +70,6 @@ function displayTime() {
             timeBlock[0].classList.remove("future")
             timeBlock[0].classList.add("past")
 
-            
         } else if (timeBlockHeadValue == hour){
             timeBlock[0].classList.remove("past") ;
             timeBlock[0].classList.remove("future") ;
@@ -89,42 +80,41 @@ function displayTime() {
             timeBlock[0].classList.remove("present") ;
             timeBlock[0].classList.add("future") ;
         }
-
     }
-
-
 }
-
-
+//---------------------------------------------------
 setInterval(displayTime, 1000)
+//---------------------------------------------------
 
+//Initialize local storage / populate page
 
+function initializePage() {
 
-//Do I need to pair each textarea element with it's own unique id?
+    if (localStorage.getItem("textareaArray") === null) {
 
-// let textareaArray = []
+        let textareaArray = []
+    
+        let numberOfTR = document.querySelectorAll("tr") ;
+    
+        for (let i=0; i< numberOfTR.length; i++) {
+            textareaArray.push({"textarea": ""})  
+        }
+    
+        localStorage.setItem("textareaArray", JSON.stringify(textareaArray))
+    } else {
+        textareaArray = JSON.parse(localStorage.getItem("textareaArray")) ;
+        let arrayTextareaEl = document.getElementsByClassName("textarea") ;
 
-//Initialize local storage
-if (localStorage.getItem("textareaArray") === null) {
-    let textareaArray = []
-    let numberOfTR = document.querySelectorAll("tr") ;
-    for (let i=0; i< numberOfTR.length; i++) {
-        textareaArray.push({"textarea": ""})  
+        for (let i = 0; i < arrayTextareaEl.length; i++) {
+            arrayTextareaEl[i].value =  textareaArray[i]["textarea"] ;
+        }
     }
-    localStorage.setItem("textareaArray", JSON.stringify(textareaArray))
 }
-
-
-
-
-// function getTextareaContent() {
-   
-// }
-
-//for each save button, create an event listener that runs local storage function for it's corresponding textarea
+//---------------------------------------------------
+initializePage() ;
+//---------------------------------------------------
 
 let arraySaveButtons = document.getElementsByClassName("saveBtn") ;
-
 
 for (let i = 0; i < arraySaveButtons.length; i++) {
     arraySaveButtons[i].addEventListener("click", function() {
@@ -132,12 +122,8 @@ for (let i = 0; i < arraySaveButtons.length; i++) {
         toLocalStorage(i) ;
     })
 }   
-//------
-// document.addEventListener("click", function(event){
-//     console.log(event.target.previousSibling) ;
-//     console.log(event.target.closest(".event"))
-// })
-//--------
+//---------------------------------------------------
+
 //Function to get content of textarea and send to local storage
 function toLocalStorage(i) {
     // console.log("save button clicked") ;
@@ -148,6 +134,7 @@ function toLocalStorage(i) {
         
         arrayTextareaContent.push(textarea.value)
     }
+    
     let areaText = arrayTextareaContent[i]
     let textareaArray = JSON.parse(localStorage.getItem("textareaArray")) ;
 
